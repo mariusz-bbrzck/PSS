f38580cb702897f54073dc88a419ade8be23bca3
mariusz-bbrzck <116429585+mariusz-bbrzck@users.noreply.github.com>
2023-04-19 20:23:40 +0200

    Add files via upload

---
+++ pss_3.1/app/controllers/HelloCtrl.class.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+
+use core\App;
+use core\Message;
+use core\Utils;
+
+
+class HelloCtrl {
+    
+    public function action_hello() {
+		        
+        $variable = 123;
+        
+        
+        App::getSmarty()->assign("value",$variable);        
+        App::getSmarty()->display("index.html");
+        
+    }
+    
+}
---
+++ pss_3.1/app/forms/RejestracjaForm.class.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\forms;
+
+class RejestracjaForm {
+	public $login;
+	public $pass;
+	public $pass2;
+	public $email;
+} 
