582ea23d15f519e5f7e9fc362b1e88857ea41ff1
mariusz-bbrzck <116429585+mariusz-bbrzck@users.noreply.github.com>
2023-04-30 23:17:31 +0200

    Delete ps_4 directory

--- ps_4/README.txt
+++
@@ -1,2 +0,0 @@
-Dodane skrypty do pliku index.html 
-Zadanie wykonane na RejestracjaCtrl.php oraz rejestracja.html
--- ps_4/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'init.php';
-use core\App;
-header("Location: ". App::getConf()->app_url);
-include App::getConf()->root_path.App::getConf()->action_script;
